42ff8060f5d77ae5feb5c925dc380bffc9b3371b
Vikash Bajaj <contactvikashbajaj@gmail.com>
2018-02-21 03:31:21 -0700

    finished base version of the template generator

---
+++ src/template_generator.py
@@ -0,0 +1,14 @@
+#! ../env/bin/python
+# -*- coding: utf-8 -*-
+
+import json
+from jinja2 import Template
+
+if __name__=="__main__":
+    config_json = json.loads(open("config.json", "r").read())
+    raw_template = open("template.jinja2", "r+").read()
+    load_template = Template(raw_template)
+    render_template = load_template.render(sources=config_json)
+    output = open("../README.md", "w")
+    output.write(render_template)
+    output.close()
