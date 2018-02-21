1752ccbd747cc6ffec3a041f1dbfc1d020edbce5
Vikash Bajaj <contactvikashbajaj@gmail.com>
2018-02-21 03:59:20 -0700

    created separate file for icons so that icons can be reused

--- src/template_generator.py
+++ src/template_generator.py
@@ -6,9 +6,10 @@ from jinja2 import Template
 
 if __name__=="__main__":
     config_json = json.loads(open("config.json", "r").read())
+    icons_json = json.loads(open("icons.json", "r").read())
     raw_template = open("template.jinja2", "r+").read()
     load_template = Template(raw_template)
-    render_template = load_template.render(sources=config_json)
+    render_template = load_template.render(sources=config_json, icons=icons_json)
     output = open("../README.md", "w")
     output.write(render_template)
     output.close()
